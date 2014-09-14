5dc0609c7eadd5fb29d0b9194cfe853709356995
Grumbo <blackwolfsden@hotmail.com>
2014-09-14 09:55:54 -0700

    Update Eluna Grumbo'z Guild Warz table.sql

--- sql/Eluna Grumbo'z Guild Warz table.sql
+++ sql/Eluna Grumbo'z Guild Warz table.sql
@@ -9,11 +9,85 @@
 /*!40101 SET NAMES utf8 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
--- Dumping data for table guild_warz.commands: ~1 rows (approximately)
+
+-- Dumping database structure for guild_warz
+CREATE DATABASE IF NOT EXISTS `guild_warz` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `guild_warz`;
+
+
+-- Dumping structure for table guild_warz.commands
+CREATE TABLE IF NOT EXISTS `commands` (
+  `guild` varchar(50) NOT NULL DEFAULT '' COMMENT 'Do Not Touch',
+  `commands` varchar(10) NOT NULL DEFAULT 'commands',
+  `info_loc` varchar(10) NOT NULL DEFAULT 'info',
+  `list_loc` varchar(10) NOT NULL DEFAULT 'list',
+  `tele` varchar(6) NOT NULL DEFAULT 'gtele',
+  `version` varchar(15) NOT NULL DEFAULT 'ver',
+  `loc` varchar(15) NOT NULL DEFAULT 'area',
+  `farm` varchar(15) NOT NULL DEFAULT 'farm',
+  `barrack` varchar(15) NOT NULL DEFAULT 'barrack',
+  `hall` varchar(15) NOT NULL DEFAULT 'hall',
+  `pig` varchar(15) NOT NULL DEFAULT 'pig',
+  `guard` varchar(15) NOT NULL DEFAULT 'guard',
+  `GLD_lvlb` tinyint(2) unsigned NOT NULL DEFAULT '0' COMMENT 'Minimum Required Guild member level to access Guild Master buy commands. Guild Master = 0 , anything over + is rank below Guild master.',
+  `GLD_lvls` tinyint(2) unsigned NOT NULL DEFAULT '0' COMMENT 'Minimum Required Guild member level to access Guild Master sell commands. Guild Master = 0 , anything over + is rank below Guild master.',
+  `respawn_flag` varchar(50) NOT NULL DEFAULT 'flag' COMMENT 'GM command spawns and assigns a flag to a guild owned area. will return an error if location is owned by SERVER',
+  `details_loc` varchar(50) NOT NULL DEFAULT 'loc' COMMENT 'GM command gives more info about location.',
+  `table` varchar(50) NOT NULL DEFAULT 'table' COMMENT 'GM command reloads GW table.',
+  `GM_admin` tinyint(3) unsigned NOT NULL DEFAULT '5' COMMENT 'GM rank of server admin',
+  `GM_minimum` tinyint(1) unsigned NOT NULL DEFAULT '4' COMMENT 'Minimum Required Gm security level to use GM level commands.',
+  `currency` bigint(20) unsigned NOT NULL DEFAULT '62006' COMMENT 'item id for currency used.',
+  `loc_cost` smallint(5) unsigned NOT NULL DEFAULT '10' COMMENT 'price for basic empty zone.',
+  `farm_cost` smallint(5) unsigned NOT NULL DEFAULT '5' COMMENT 'price for small cheezy government issued house.',
+  `barrack_cost` smallint(5) unsigned NOT NULL DEFAULT '10' COMMENT 'price for small cheezy government issued house.',
+  `hall_cost` smallint(5) unsigned NOT NULL DEFAULT '100' COMMENT 'price for small cheezy government issued house.',
+  `pig_cost` smallint(5) NOT NULL DEFAULT '1' COMMENT 'price for one of these porkerz',
+  `guard_cost` smallint(5) NOT NULL DEFAULT '1' COMMENT 'how much is your back worth guarding?',
+  `farm_L` smallint(5) NOT NULL DEFAULT '1' COMMENT 'how many houses per location.',
+  `barrack_L` smallint(5) NOT NULL DEFAULT '1' COMMENT 'how many houses per location.',
+  `hall_L` smallint(5) NOT NULL DEFAULT '1' COMMENT 'how many houses per location.',
+  `pig_L` smallint(5) NOT NULL DEFAULT '20' COMMENT 'how many pigs per location.',
+  `guard_L` smallint(5) NOT NULL DEFAULT '10' COMMENT 'how many guards per location.',
+  `pig_payz` bigint(20) NOT NULL DEFAULT '100000' COMMENT 'amount in copper that each pig payz. hence "pig payz". default 100000 = 10g ',
+  `pig_payz_timer` bigint(20) NOT NULL DEFAULT '1800000' COMMENT 'timer for pig payz. default  1800000 = 30 minutes.',
+  `gift_count` tinyint(3) NOT NULL DEFAULT '25' COMMENT 'how many of currency item to give to guildmaster of newly created guild.',
+  `flag_require` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'does capturing a flag require killing all guards? default  == 0 no ; 1 == yes',
+  `Server` varchar(6) NOT NULL DEFAULT 'SERVER' COMMENT 'Do Not Touch',
+  `flag_id` mediumint(8) NOT NULL DEFAULT '187432' COMMENT 'Do Not Touch',
+  `farm_id` mediumint(8) NOT NULL DEFAULT '500000' COMMENT 'Do Not Touch',
+  `barrack_id` mediumint(8) NOT NULL DEFAULT '500002' COMMENT 'Do Not Touch',
+  `hall_id` mediumint(8) NOT NULL DEFAULT '500004' COMMENT 'Do Not Touch',
+  `pig_id` mediumint(8) NOT NULL DEFAULT '49000' COMMENT 'Do Not Touch',
+  `guard_id` mediumint(8) NOT NULL DEFAULT '49002' COMMENT 'Do Not Touch',
+  `x1` mediumint(8) NOT NULL DEFAULT '0' COMMENT 'Do Not Touch',
+  `x2` mediumint(8) NOT NULL DEFAULT '0' COMMENT 'Do Not Touch',
+  `x3` mediumint(8) NOT NULL DEFAULT '0' COMMENT 'Do Not Touch',
+  `command_set` varchar(50) NOT NULL DEFAULT 'set' COMMENT 'Do Not Touch ',
+  `anarchy` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'pure GvG PvP no faction filter.',
+  `f_timer` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'applies a cooldown-timer when a flag spaws. 0=off::1=on',
+  `s_timer` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT 'no-tag-flag timer cooldown time in seconds. i.e. 5 minutes = 300 seconds.',
+  `guild_id` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'guild id',
+  `guild_invite` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT 'guild invite system. 0 == off :: 1 == on',
+  PRIMARY KEY (`guild`),
+  UNIQUE KEY `guild` (`guild`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table guild_warz.commands: ~2 rows (approximately)
 /*!40000 ALTER TABLE `commands` DISABLE KEYS */;
 REPLACE INTO `commands` (`guild`, `commands`, `info_loc`, `list_loc`, `tele`, `version`, `loc`, `farm`, `barrack`, `hall`, `pig`, `guard`, `GLD_lvlb`, `GLD_lvls`, `respawn_flag`, `details_loc`, `table`, `GM_admin`, `GM_minimum`, `currency`, `loc_cost`, `farm_cost`, `barrack_cost`, `hall_cost`, `pig_cost`, `guard_cost`, `farm_L`, `barrack_L`, `hall_L`, `pig_L`, `guard_L`, `pig_payz`, `pig_payz_timer`, `gift_count`, `flag_require`, `Server`, `flag_id`, `farm_id`, `barrack_id`, `hall_id`, `pig_id`, `guard_id`, `x1`, `x2`, `x3`, `command_set`, `anarchy`, `f_timer`, `s_timer`, `guild_id`, `guild_invite`) VALUES
 	('SERVER', 'commands', 'info', 'list', 'gtele', 'ver', 'area', 'farm', 'barrack', 'hall', 'pig', 'guard', 0, 0, 'flag', 'loc', 'table', 5, 4, 62006, 10, 5, 10, 100, 1, 1, 1, 1, 1, 20, 10, 100000, 1800000, 25, 0, 'SERVER', 187432, 500000, 500002, 500005, 49000, 49002, 1, 1, 1, 'set', 1, 1, 300, 0, 0);
 /*!40000 ALTER TABLE `commands` ENABLE KEYS */;
+
+
+-- Dumping structure for table guild_warz.help
+CREATE TABLE IF NOT EXISTS `help` (
+  `entry` bigint(20) unsigned DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `description` varchar(1028) DEFAULT NULL,
+  `example` varchar(1028) DEFAULT NULL,
+  `command_level` varchar(1028) DEFAULT NULL,
+  UNIQUE KEY `entry` (`entry`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Dumping data for table guild_warz.help: ~60 rows (approximately)
 /*!40000 ALTER TABLE `help` DISABLE KEYS */;
@@ -80,7 +154,30 @@ REPLACE INTO `help` (`entry`, `name`, `description`, `example`, `command_level`)
 	(60, 'guild_invite', 'Guild Level Setting. Guild invite system 0 == off :: 1 == on // default on.', 'set guild_invite 1', '4');
 /*!40000 ALTER TABLE `help` ENABLE KEYS */;
 
--- Dumping data for table guild_warz.zones: ~1 rows (approximately)
+
+-- Dumping structure for table guild_warz.zones
+CREATE TABLE IF NOT EXISTS `zones` (
+  `entry` bigint(10) unsigned NOT NULL DEFAULT '0',
+  `map_id` bigint(10) unsigned NOT NULL DEFAULT '0',
+  `area_id` bigint(10) unsigned NOT NULL DEFAULT '0',
+  `zone_id` bigint(10) unsigned NOT NULL DEFAULT '0',
+  `guild_name` varchar(50) NOT NULL DEFAULT 'SERVER',
+  `team` smallint(10) unsigned NOT NULL DEFAULT '2',
+  `x` float NOT NULL DEFAULT '0',
+  `y` float NOT NULL DEFAULT '0',
+  `z` float NOT NULL DEFAULT '0',
+  `farm_count` tinyint(10) unsigned NOT NULL DEFAULT '0',
+  `barrack_count` tinyint(10) unsigned NOT NULL DEFAULT '0',
+  `hall_count` tinyint(10) unsigned NOT NULL DEFAULT '0',
+  `pig_count` tinyint(10) unsigned NOT NULL DEFAULT '0',
+  `guard_count` tinyint(10) unsigned NOT NULL DEFAULT '0',
+  `flag_id` bigint(20) NOT NULL DEFAULT '0',
+  `fs_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'time in seconds when the flag was spawned.',
+  `guild_id` mediumint(8) NOT NULL DEFAULT '0' COMMENT 'guild id',
+  UNIQUE KEY `entry` (`entry`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table guild_warz.zones: ~3 rows (approximately)
 /*!40000 ALTER TABLE `zones` DISABLE KEYS */;
 REPLACE INTO `zones` (`entry`, `map_id`, `area_id`, `zone_id`, `guild_name`, `team`, `x`, `y`, `z`, `farm_count`, `barrack_count`, `hall_count`, `pig_count`, `guard_count`, `flag_id`, `fs_time`, `guild_id`) VALUES
 	(1, 0, 0, 0, 'SERVER', 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
