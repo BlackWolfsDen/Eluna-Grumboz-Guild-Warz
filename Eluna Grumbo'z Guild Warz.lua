f68b7d3e7313fea9c95e90d01768fb849eff0333
Grumbo <blackwolfsden@hotmail.com>
2014-09-16 19:06:35 -0700

    Update Eluna Grumbo'z Guild Warz.lua

--- Eluna Grumbo'z Guild Warz.lua
+++ Eluna Grumbo'z Guild Warz.lua
@@ -13,6 +13,8 @@
 -- *b* Please Do Not Remove any of the credits ***
 -- ****** or attempt to repost as your own **2****
 -- ***o********************************0**********
+-- *********** release date 03-10-2013 ***********
+-- ***********************************************
 
 print("\n-----------------------------------")
 print("Grumbo'z Guild Warz System Loading:\n")
