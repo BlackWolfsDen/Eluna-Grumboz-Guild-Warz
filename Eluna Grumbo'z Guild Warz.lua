6f33c0875bed1c28b607165edc24e18c67d8a8de
Grumbo <blackwolfsden@hotmail.com>
2014-11-09 12:15:22 -0700

    Update Eluna Grumbo'z Guild Warz.lua

--- Eluna Grumbo'z Guild Warz.lua
+++ Eluna Grumbo'z Guild Warz.lua
@@ -39,7 +39,7 @@ local tele_version = 1.50
 local pvp_version = 4.60
 
 local Server = "SERVER"
-local guild_warz_DB = "guild_warz_335a" -- use unique name if running on multiple cores i.e. guild_warz_3.3.5a_1 
+local guild_warz_DB = "guild_warz_335a" -- Must match unique name if running on multiple cores i.e. guild_warz_3.3.5a_1 
 
 GWCOMM = {};
 GWARZ = {};
