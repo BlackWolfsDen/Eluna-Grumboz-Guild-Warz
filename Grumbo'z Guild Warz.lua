c98bac7afd63b34d419d87b5dee5b63b2b976d0d
Grumbo <blackwolfsden@hotmail.com>
2015-02-15 13:01:36 -0700

    Update Grumbo'z Guild Warz.lua

--- Grumbo'z Guild Warz.lua
+++ Grumbo'z Guild Warz.lua
@@ -41,15 +41,15 @@ local tele_version = 1.50;
 local pvp_version = 4.60;
 local vendor_version = 1.50;
 
+local Server = "SERVER";
+local guild_warz_DB = "guild_warz_335"; -- Must match unique name if running on multiple cores i.e. guild_warz_335_1 
+
 -- -----------------------------------------------------
 -- built-in vendors operational switches and item tables
 -- -----------------------------------------------------
 local vendor1 = 1; -- use built-in vendor 1 0/1 no/yes.
 local vendor2 = 1; -- use built-in vendor 2 0/1 no/yes.
 local vendor3 = 1; -- use built-in vendor 3 0/1 no/yes.
-
-local Server = "SERVER";
-local guild_warz_DB = "guild_warz_335"; -- Must match unique name if running on multiple cores i.e. guild_warz_335_1 
 
 -- vendor items {item_id, custom_cost}, if no custom cost then use 0.(vendor item limit = 10 items per 15 pages)
 local Vendor2 = {{7734,0},{6948,0},{49912,0},{34498,0},{46693,0},{34499,0},{35557,0},{37431,0},{17202,0},{21038,0},{46783,0},}; -- funny items
