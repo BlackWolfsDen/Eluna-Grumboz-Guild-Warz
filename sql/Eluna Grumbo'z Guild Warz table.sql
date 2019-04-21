ab9da2e2c1a1599c182890030502f10ab789f7b1
Grumbo <phillipbalazs@hotmail.com>
2019-04-20 21:28:04 -0600

    updated sql db to latest

--- sql/Eluna Grumbo'z Guild Warz table.sql
+++ sql/Eluna Grumbo'z Guild Warz table.sql
@@ -5,14 +5,13 @@
 -- HeidiSQL Version:             9.1.0.4867
 -- Grumboz Guild Warz            3.3.5a WotlK Eluna Boost
 -- --------------------------------------------------------
-CREATE DATABASE IF NOT EXISTS `guild_warz_335` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE IF NOT EXISTS `guild_warz_335` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `guild_warz_335`;
-
 
 CREATE TABLE IF NOT EXISTS `commands` (
   `guild` varchar(50) NOT NULL DEFAULT '' COMMENT 'Do Not Touch',
-  `guild_id` mediumint(8) unsigned NULL DEFAULT NULL COMMENT 'guild id',
-  `team` tinyint(1) unsigned NOT NULL COMMENT 'guild team id',
+  `guild_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'guild id',
+  `team` TINYINT(4) NOT NULL DEFAULT '2', 
   `commands` varchar(10) NOT NULL DEFAULT 'commands',
   `info_loc` varchar(10) NOT NULL DEFAULT 'info',
   `list_loc` varchar(10) NOT NULL DEFAULT 'list',
@@ -25,7 +24,7 @@ CREATE TABLE IF NOT EXISTS `commands` (
   `table` varchar(50) NOT NULL DEFAULT 'table' COMMENT 'GM command reloads GW table.',
   `GM_admin` tinyint(3) unsigned NOT NULL DEFAULT '4' COMMENT 'GM rank of server admin',
   `GM_minimum` tinyint(1) unsigned NOT NULL DEFAULT '3' COMMENT 'Minimum Required Gm security level to use GM level commands.',
-  `currency` mediumint(8) unsigned NOT NULL DEFAULT '62006' COMMENT 'item id for currency used.',
+  `currency` bigint(20) unsigned NOT NULL DEFAULT '62006' COMMENT 'item id for currency used.',
   `pig_payz` bigint(20) unsigned NOT NULL DEFAULT '100000' COMMENT 'amount in copper that each pig payz. hence "pig payz". default 100000 = 10g ',
   `pig_payz_timer` bigint(20) unsigned NOT NULL DEFAULT '1800000' COMMENT 'timer for pig payz. default  1800000 = 30 minutes.',
   `gift_count` smallint(5) unsigned NOT NULL DEFAULT '25' COMMENT 'how many of currency item to give to guildmaster of newly created guild.',
@@ -34,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `commands` (
   `command_set` varchar(50) NOT NULL DEFAULT 'set' COMMENT 'Do Not Touch ',
   `anarchy` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'pure GvG PvP no faction filter.',
   `f_timer` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'applies a cooldown-timer when a flag spaws. 0=off::1=on',
-  `s_timer` bigint(20) unsigned NOT NULL DEFAULT '600000' COMMENT 'no-tag-flag timer cooldown time in seconds. i.e. 5 minutes = 300 seconds.',
+  `s_timer` bigint(20) unsigned NOT NULL DEFAULT '300' COMMENT 'no-tag-flag timer cooldown time in seconds. i.e. 5 minutes = 300 seconds.',
   `guild_invite` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'guild invite system. 0 == off :: 1 == on',
   `loc` varchar(15) NOT NULL DEFAULT 'area',
   `loc_cost` smallint(5) unsigned NOT NULL DEFAULT '10' COMMENT 'price for basic empty zone.',
@@ -83,12 +82,12 @@ CREATE TABLE IF NOT EXISTS `commands` (
   `mailbox_cost` smallint(5) unsigned NOT NULL DEFAULT '10',
   `mailbox_L` smallint(5) unsigned NOT NULL DEFAULT '1',
   `mailbox_id` bigint(20) unsigned NOT NULL DEFAULT '500010' COMMENT 'Do Not Touch',
-  `setup` varchar(10) DEFAULT 'setup' COMMENT 'player command that gives info about how its setup.',
-  `color_1` varchar(10) DEFAULT '|cff00cc00' COMMENT 'Command color GREEN',
+  `setup` VARCHAR(50) NOT NULL DEFAULT '500010' COMMENT 'command used to list commands',
+  `color_1` varchar(10) NOT NULL DEFAULT '|cff00cc00' COMMENT 'Command color GREEN',
   `color_2` varchar(10) NOT NULL DEFAULT '|cffFFFF00' COMMENT 'Value color YELLOW',
   `color_3` varchar(10) NOT NULL DEFAULT '|cffFFFFFF' COMMENT 'Info color WHITE',
   `color_4` varchar(10) NOT NULL DEFAULT '|cff3399FF' COMMENT 'Alliance color BLUE',
-  `color_5` varchar(10) NOT NULL DEFAULT '|cffBA0000' COMMENT 'Horde color RED',
+  `color_5` varchar(10) NOT NULL DEFAULT '|cffFF0000' COMMENT 'Horde color RED',
   `color_6` varchar(10) NOT NULL DEFAULT '|cffFFFF00' COMMENT 'For Sale color YELLOW',
   `color_7` varchar(10) NOT NULL DEFAULT '|cffFF0000' COMMENT 'LOCKED color RED',
   `color_8` varchar(10) NOT NULL DEFAULT '|cffC0C0C0' COMMENT 'Titles color GREY',
@@ -99,21 +98,23 @@ CREATE TABLE IF NOT EXISTS `commands` (
   `color_13` varchar(10) NOT NULL DEFAULT '|cffFFFFFF' COMMENT 'SET Values color WHITE',
   `color_14` varchar(10) NOT NULL DEFAULT '|cff00cc00' COMMENT 'Good Announcements color GREEN',
   `color_15` varchar(10) NOT NULL DEFAULT '|cffFF0000' COMMENT 'Bad Annoucements RED',
-  UNIQUE KEY `guild_id` (`guild_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='version 1.77';
+  PRIMARY KEY (`guild`),
+  UNIQUE KEY `guild` (`guild`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='version 1.76';
 
-REPLACE INTO `commands` (`guild`, `guild_id`, `team`, `commands`, `info_loc`, `list_loc`, `tele`, `version`, `GLD_lvlb`, `GLD_lvls`, `respawn_flag`, `details_loc`, `table`, `GM_admin`, `GM_minimum`, `currency`, `pig_payz`, `pig_payz_timer`, `gift_count`, `flag_require`, `Server`, `command_set`, `anarchy`, `f_timer`, `s_timer`, `guild_invite`, `loc`, `loc_cost`, `flag_id`, `farm`, `farm_cost`, `farm_L`, `farm_id`, `barrack`, `barrack_cost`, `barrack_L`, `barrack_id`, `hall`, `hall_cost`, `hall_L`, `hall_id`, `pig`, `pig_cost`, `pig_L`, `pig_id`, `guard`, `guard_cost`, `guard_L`, `guard_id`, `vendor1`, `vendor1_cost`, `vendor1_L`, `vendor1_id`, `vendor2`, `vendor2_cost`, `vendor2_L`, `vendor2_id`, `vendor3`, `vendor3_cost`, `vendor3_L`, `vendor3_id`, `cannon`, `cannon_cost`, `cannon_L`, `cannon_id`, `vault`, `vault_cost`, `vault_L`, `vault_id`, `mailbox`, `mailbox_cost`, `mailbox_L`, `mailbox_id`, `setup`, `color_1`, `color_2`, `color_3`, `color_4`, `color_5`, `color_6`, `color_7`, `color_8`, `color_9`, `color_10`, `color_11`, `color_12`, `color_13`, `color_14`, `color_15`) VALUES
-	('SERVER', 0, 0, 'commands', 'info', 'list', 'gtele', 'ver', 0, 0, 'flag', 'loc', 'table', 5, 4, 62006, 100000, 1800000, 25, 0, 'SERVER', 'set', 1, 1, 300, 0, 'area', 10, 187432, 'farm', 5, 3, 500000, 'barrack', 10, 1, 500002, 'hall', 100, 1, 500004, 'pig', 1, 5, 49000, 'guard', 1, 10, 49002, 'vendor1', 25, 1, 49004, 'vendor2', 25, 1, 49006, 'vendor3', 25, 1, 49008, 'cannon', 100, 3, 49010, 'vault', 100, 3, 500006, 'mailbox', 10, 3, 500008, 'setup', '|cff00cc00', '|cffFFFF00', '|cffFFFFFF', '|cff3399FF', '|cffFF0000', '|cffFFFF00', '|cffFF0000', '|cffC0C0C0', '|cff000000', '|cff00cc00', '|cffFFFF00', '|cffFF0000', '|cffFFFFFF', '|cff00cc00', '|cffFF0000');
+REPLACE INTO `commands` (`guild`, `commands`, `info_loc`, `list_loc`, `tele`, `version`, `GLD_lvlb`, `GLD_lvls`, `respawn_flag`, `details_loc`, `table`, `GM_admin`, `GM_minimum`, `currency`, `pig_payz`, `pig_payz_timer`, `gift_count`, `flag_require`, `Server`, `command_set`, `anarchy`, `f_timer`, `s_timer`, `guild_id`, `guild_invite`, `loc`, `loc_cost`, `flag_id`, `farm`, `farm_cost`, `farm_L`, `farm_id`, `barrack`, `barrack_cost`, `barrack_L`, `barrack_id`, `hall`, `hall_cost`, `hall_L`, `hall_id`, `pig`, `pig_cost`, `pig_L`, `pig_id`, `guard`, `guard_cost`, `guard_L`, `guard_id`, `vendor1`, `vendor1_cost`, `vendor1_L`, `vendor1_id`, `vendor2`, `vendor2_cost`, `vendor2_L`, `vendor2_id`, `vendor3`, `vendor3_cost`, `vendor3_L`, `vendor3_id`, `cannon`, `cannon_cost`, `cannon_L`, `cannon_id`, `vault`, `vault_cost`, `vault_L`, `vault_id`, `mailbox`, `mailbox_cost`, `mailbox_L`, `mailbox_id`, `color_1`, `color_2`, `color_3`, `color_4`, `color_5`, `color_6`, `color_7`, `color_8`, `color_9`, `color_10`, `color_11`, `color_12`, `color_13`, `color_14`, `color_15`) VALUES
+	('Liya', 'commands', 'info', 'list', 'gtele', 'ver', 0, 0, 'flag', 'loc', 'table', 5, 4, 62006, 100000, 1800000, 25, 0, 'SERVER', 'set', 0, 0, 300, 4, 0, 'area', 10, 187432, 'farm', 5, 1, 500000, 'barrack', 10, 1, 500002, 'hall', 100, 1, 500004, 'pig', 1, 20, 49000, 'guard', 1, 10, 49002, 'vendor1', 25, 1, 49004, 'vendor2', 25, 1, 49006, 'vendor3', 25, 1, 49008, 'cannon', 100, 3, 49010, 'vault', 100, 3, 500006, 'mailbox', 10, 3, 500008, '|cff00cc00', '|cffFFFF00', '|cffFFFFFF', '|cff3399FF', '|cffFF0000', '|cffFFFF00', '|cffFF0000', '|cffC0C0C0', '|cff000000', '|cff00cc00', '|cffFFFF00', '|cffFF0000', '|cffFFFFFF', '|cff00cc00', '|cffFF0000'),
+	('SERVER', 'commands', 'info', 'list', 'gtele', 'ver', 0, 0, 'flag', 'loc', 'table', 5, 4, 62006, 100000, 1800000, 25, 0, 'SERVER', 'set', 1, 1, 300, 0, 0, 'area', 10, 187432, 'farm', 5, 3, 500000, 'barrack', 10, 1, 500002, 'hall', 100, 1, 500004, 'pig', 1, 5, 49000, 'guard', 1, 10, 49002, 'vendor1', 25, 1, 49004, 'vendor2', 25, 1, 49006, 'vendor3', 25, 1, 49008, 'cannon', 100, 3, 49010, 'vault', 100, 3, 500006, 'mailbox', 10, 3, 500008, '|cff00cc00', '|cffFFFF00', '|cffFFFFFF', '|cff3399FF', '|cffFF0000', '|cffFFFF00', '|cffFF0000', '|cffC0C0C0', '|cff000000', '|cff00cc00', '|cffFFFF00', '|cffFF0000', '|cffFFFFFF', '|cff00cc00', '|cffFF0000');
 
 CREATE TABLE IF NOT EXISTS `help` (
-  `entry` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `entry` bigint(20) unsigned NOT NULL DEFAULT '0',
   `name` varchar(50) DEFAULT NULL,
   `description` varchar(1028) NOT NULL,
   `example` varchar(1028) NOT NULL,
   `command_level` varchar(1028) NOT NULL,
   PRIMARY KEY (`entry`),
   UNIQUE KEY `entry` (`entry`)
-) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=utf8 MAX_ROWS=5000 COMMENT='version 1.77';
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='version 1.76';
 
 REPLACE INTO `help` (`entry`, `name`, `description`, `example`, `command_level`) VALUES
 	(1, 'guild', 'Guild name for command entry . non-modifyable >>>>>DO NOT CHANGE<<<<<<', 'LOCKED', '7'),
@@ -206,47 +207,34 @@ REPLACE INTO `help` (`entry`, `name`, `description`, `example`, `command_level`)
 	(88, 'color_12', 'Set Error color  GuildMaster Level HEX CODE', 'set color_12 cff000000', '4'),
 	(89, 'color_13', 'Set Values color  GuildMaster Level HEX CODE', 'set color_13 cff000000', '4'),
 	(90, 'color_14', 'Good Announcements  color  GuildMaster Level HEX CODE', 'set color_14 cff000000', '4'),
-	(91, 'color_15', 'Bad Announcements  color  GuildMaster Level HEX CODE', 'set color_15 cff000000', '4'),
-	(92, 'reset barrack', 'allows GM of minimum rank to reset barrack count of a location to 0', 'LOCKED', '7'),
-	(93, 'reset hall', 'allows GM of minimum rank to reset hall count of a location to 0', 'LOCKED', '7'),
-	(94, 'reset pig', 'allows GM of minimum rank to reset pig countof a location to 0', 'LOCKED', '7'),
-	(95, 'reset guard', 'allows GM of minimum rank to reset guard count of a location to 0', 'LOCKED', '7'),
-	(96, 'reset vendor1', 'allows GM of minimum rank to reset vendor_1 count of a location to 0', 'LOCKED', '7'),
-	(97, 'reset vendor2', 'allows GM of minimum rank to reset vendor_2 count of a location to 0', 'LOCKED', '7'),
-	(98, 'reset vendor3', 'allows GM of minimum rank to reset vendor_3 count of a location to 0', 'LOCKED', '7'),
-	(99, 'reset cannon', 'allows GM of minimum rank to reset cannon count of a location to 0', 'LOCKED', '7'),
-	(100, 'reset vault', 'allows GM of minimum rank to reset vault count of a location to 0', 'LOCKED', '7'),
-	(101, 'reset mailbox', 'allows GM of minimum rank to reset mailbox count of a location to 0', 'LOCKED', '7'),
-	(102, 'lock', 'ADMIN command to lock an area from purchase.   >>>>>DO NOT CHANGE<<<<<', 'LOCKED', '7'),
-	(103, 'setup', 'fixed player command .', 'LOCKED', '7');
-/*!40000 ALTER TABLE `help` ENABLE KEYS */;
+	(91, 'color_15', 'Bad Announcements  color  GuildMaster Level HEX CODE', 'set color_15 cff000000', '4');
 
 CREATE TABLE IF NOT EXISTS `zones` (
-  `entry` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `map_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `area_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `zone_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `guild_name` varchar(50) NOT NULL DEFAULT 'SERVER',
+  `entry` bigint(10) unsigned NOT NULL DEFAULT '0',
+  `map_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `area_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `zone_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `guild_name` char(50) NOT NULL DEFAULT 'SERVER',
   `team` tinyint(1) unsigned NOT NULL DEFAULT '2',
   `x` float NOT NULL DEFAULT '0',
   `y` float NOT NULL DEFAULT '0',
   `z` float NOT NULL DEFAULT '0',
-  `farm_count` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `barrack_count` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `hall_count` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `pig_count` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `guard_count` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `vendor1_count` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `vendor2_count` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `vendor3_count` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `cannon_count` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `vault_count` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `mailbox_count` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `flag_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `farm_count` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `barrack_count` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `hall_count` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `pig_count` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `guard_count` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `vendor1_count` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `vendor2_count` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `vendor3_count` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `cannon_count` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `vault_count` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `mailbox_count` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `flag_id` bigint(20) unsigned NOT NULL DEFAULT '0',
   `fs_time` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'game time (in seconds) when the flag was spawned.',
-  `guild_id` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT 'guild id',
+  `guild_id` bigint(20) unsigned NOT NULL DEFAULT '0' COMMENT 'guild id',
   PRIMARY KEY (`entry`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='version 1.76';
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='version 1.76';
 
 REPLACE INTO `zones` (`entry`, `map_id`, `area_id`, `zone_id`, `guild_name`, `team`, `x`, `y`, `z`, `farm_count`, `barrack_count`, `hall_count`, `pig_count`, `guard_count`, `vendor1_count`, `vendor2_count`, `vendor3_count`, `cannon_count`, `vault_count`, `mailbox_count`, `flag_id`, `fs_time`, `guild_id`) VALUES
 	(1, 0, 0, 0, 'SERVER', 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
